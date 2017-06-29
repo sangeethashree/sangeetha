ca0eee8d79203e3d76391c58dd4fdf39f6ab6de1
sangeetha <sangeethashree195@gmail.com>
2017-06-29 15:50:14 +0530

    Create pro.java

---
+++ pro.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class pro{
+public static void main(String[] args){
+int s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextInt();
+int r=s%2;
+if(r==0)
+{
+System.out.println("The Given Number is Even");
+}
+else
+{
+System.out.println("The Given Number is Odd");
+}
+}
+}
