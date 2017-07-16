1eb59cb7b76d446db4a449eb4766d5cf4e42fef2
sangeetha <sangeethashree195@gmail.com>
2017-07-16 10:39:04 +0530

    Update Hlo.java

--- Hlo.java
+++ Hlo.java
@@ -3,10 +3,14 @@ import java.io.*;
 public class Hlo{
 public static void main(String[] args)
 {
+  String a;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the word");
+  a=sc.a nextLine();
 int i;
 for(i=0;i<5;i++)
 {
-System.out.println("Hello World");
+System.out.println(a);
 }
 }
 }
